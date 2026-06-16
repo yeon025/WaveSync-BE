74aae37ff84bf029cc613771e4e6a3df056693f7
박소연 <soyeon6527@gmail.com>
2026-06-16 14:26:21 +0900

    chore: 프로젝트 초기 설정 완료

---
+++ spring/src/main/java/io/github/wavesync/WavesyncApplication.java
@@ -0,0 +1,13 @@
+package io.github.wavesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavesyncApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/io/github/wavesync/WavesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wavesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
