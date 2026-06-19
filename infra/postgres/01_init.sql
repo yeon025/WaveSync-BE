18ff2337e8776e3e16527a81a2a5dff06a115e6f
박소연 <soyeon6527@gmail.com>
2026-06-19 13:26:40 +0900

    feat: PostgreSQL init.sql 적용 및 DB 레이어 구성

---
+++ infra/postgres/01_init.sql
@@ -0,0 +1,194 @@
+-- =========================
+-- weapon_master
+-- =========================
+CREATE TABLE IF NOT EXISTS weapon_master (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    attack_value INT NOT NULL,
+    main_type VARCHAR(20) NOT NULL,
+    main_value NUMERIC(5,2) NOT NULL,
+    refine_type VARCHAR(20),
+    refine_1_value INT,
+    refine_2_value INT,
+    refine_3_value INT,
+    refine_4_value INT,
+    refine_5_value INT,
+    image VARCHAR(255) NOT NULL
+);
+
+
+-- =========================
+-- resonator_master
+-- =========================
+CREATE TABLE IF NOT EXISTS resonator_master (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(20) NOT NULL UNIQUE,
+    element VARCHAR(10) NOT NULL,
+    rarity INT NOT NULL CHECK (rarity IN (4, 5)),
+    hp INT NOT NULL,
+    attack INT NOT NULL,
+    defense INT NOT NULL,
+    release_version INT NOT NULL,
+    thumbnail_image VARCHAR(255) NOT NULL,
+    standing_image VARCHAR(255)
+);
+
+
+-- =========================
+-- resonance_node_master
+-- =========================
+CREATE TABLE IF NOT EXISTS resonance_node_master (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    outer_node_type VARCHAR(20) NOT NULL,
+    outer_top_node_value NUMERIC(5,2) NOT NULL,
+    outer_middle_node_value NUMERIC(5,2) NOT NULL,
+
+    inner_node_type VARCHAR(20) NOT NULL,
+    inner_top_node_value NUMERIC(5,2) NOT NULL,
+    inner_middle_node_value NUMERIC(5,2) NOT NULL,
+
+    resonator_master_id INT NOT NULL UNIQUE,
+
+    CONSTRAINT fk_resonance_node_resonator
+        FOREIGN KEY (resonator_master_id)
+        REFERENCES resonator_master(id)
+);
+
+
+-- =========================
+-- echo_master
+-- =========================
+CREATE TABLE IF NOT EXISTS echo_master (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    
+    name VARCHAR(50) NOT NULL UNIQUE,
+    image VARCHAR(255) NOT NULL
+);
+
+
+-- =========================
+-- user_resonators
+-- =========================
+CREATE TABLE IF NOT EXISTS user_resonators (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    resonance_chain_level INT NOT NULL CHECK (resonance_chain_level BETWEEN 0 AND 6),
+    refine_level INT NOT NULL CHECK (refine_level BETWEEN 1 AND 5),
+
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+
+    resonator_master_id INT NOT NULL,
+    weapon_master_id INT NOT NULL,
+
+    CONSTRAINT fk_user_resonator_master
+        FOREIGN KEY (resonator_master_id)
+        REFERENCES resonator_master(id),
+
+    CONSTRAINT fk_user_weapon
+        FOREIGN KEY (weapon_master_id)
+        REFERENCES weapon_master(id)
+);
+
+
+-- =========================
+-- user_echoes
+-- =========================
+CREATE TABLE IF NOT EXISTS user_echoes (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    main_type VARCHAR(20) NOT NULL,
+    main_value NUMERIC(5,2) NOT NULL,
+
+    secondary_type VARCHAR(20) NOT NULL,
+    secondary_value INT NOT NULL,
+		
+		echo_master_id INT NOT NULL,
+    user_resonator_id INT NOT NULL,
+
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    
+    CONSTRAINT fk_user_echoes_echo_master
+        FOREIGN KEY (echo_master_id)
+        REFERENCES echo_master(id),
+
+    CONSTRAINT fk_user_echo_resonator
+        FOREIGN KEY (user_resonator_id)
+        REFERENCES user_resonators(id)
+);
+
+
+-- =========================
+-- user_echo_sub
+-- =========================
+CREATE TABLE IF NOT EXISTS user_echo_sub (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    type VARCHAR(20) NOT NULL,
+    value NUMERIC(7,2) NOT NULL,
+
+    unit VARCHAR(20) NOT NULL CHECK (unit IN ('flat', 'percent')),
+
+    user_echo_id INT NOT NULL,
+
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+
+    CONSTRAINT fk_echo_sub
+        FOREIGN KEY (user_echo_id)
+        REFERENCES user_echoes(id)
+);
+
+
+-- =========================
+-- user_resonance_nodes
+-- =========================
+CREATE TABLE IF NOT EXISTS user_resonance_nodes (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    branch_position VARCHAR(10) NOT NULL CHECK (branch_position IN ('top', 'middle')),
+    node_position VARCHAR(10) NOT NULL CHECK (node_position IN ('outer', 'inner')),
+
+    is_active BOOLEAN NOT NULL DEFAULT FALSE,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+
+    user_resonator_id INT NOT NULL,
+
+    CONSTRAINT fk_user_node_resonator
+        FOREIGN KEY (user_resonator_id)
+        REFERENCES user_resonators(id)
+);
+
+
+-- =========================
+-- final_stats
+-- =========================
+CREATE TABLE IF NOT EXISTS final_stats (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+    hp INT NOT NULL,
+    attack INT NOT NULL,
+    defense INT NOT NULL,
+
+    resonance_efficiency NUMERIC(5,2) NOT NULL,
+    critical NUMERIC(5,2) NOT NULL,
+    critical_damage NUMERIC(5,2) NOT NULL,
+
+    resonance_skill_bonus NUMERIC(5,2) NOT NULL,
+    basic_attack_bonus NUMERIC(5,2) NOT NULL,
+    heavy_attack_bonus NUMERIC(5,2) NOT NULL,
+    resonance_liberation_bonus NUMERIC(5,2) NOT NULL,
+
+    glacio_bonus NUMERIC(5,2) NOT NULL,
+    fusion_bonus NUMERIC(5,2) NOT NULL,
+    conducto_bonus NUMERIC(5,2) NOT NULL,
+    aero_bonus NUMERIC(5,2) NOT NULL,
+    spectro_bonus NUMERIC(5,2) NOT NULL,
+    havoc_bonus NUMERIC(5,2) NOT NULL,
+    healing_bonus NUMERIC(5,2) NOT NULL,
+
+    user_resonator_id INT NOT NULL,
+
+    CONSTRAINT fk_final_stats_resonator
+        FOREIGN KEY (user_resonator_id)
+        REFERENCES user_resonators(id)
+);
