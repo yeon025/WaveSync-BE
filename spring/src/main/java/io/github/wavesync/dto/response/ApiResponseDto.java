474065a8157b3880ef7acdfb7c30afe3c34f74b3
yeon025 <soyeon6527@gmail.com>
2026-06-24 19:06:27 +0900

    feat: 컨트롤러,  서비스 기본 구조 생성

--- spring/src/main/java/io/github/wavesync/dto/response/ApiResponseDto.java
+++ spring/src/main/java/io/github/wavesync/dto/response/ApiResponseDto.java
@@ -1,22 +1,21 @@
 package io.github.wavesync.dto.response;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-
+import org.springframework.http.HttpStatus;
 
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApiResponseDto<T> {
-    private String code;
+    private HttpStatus code;
     private String message;
     private T data;
 
-    public static <T> ApiResponseDto<T> of(String code, String message, T data) {
+    public static <T> ApiResponseDto<T> of(HttpStatus code, String message, T data) {
         return new ApiResponseDto<>(code, message, data);
     }
 
-    public static <T> ApiResponseDto<T> of(String code, String message) {
+    public static ApiResponseDto<Void> of(HttpStatus code, String message) {
         return new ApiResponseDto<>(code, message, null);
     }
 }
--- spring/src/main/java/io/github/wavesync/service/ResonatorService.java
+++ spring/src/main/java/io/github/wavesync/service/ResonatorService.java
@@ -1,5 +1,41 @@
 package io.github.wavesync.service;
+import io.github.wavesync.dto.request.DeleteResonatorRequestDto;
+import io.github.wavesync.dto.request.UpdateResonatorRequestDto;
+import io.github.wavesync.dto.response.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+
+@Service
+@RequiredArgsConstructor
 public class ResonatorService {
 
+
+
+    public CreateResonatorResponseDto createResonator(MultipartFile resonatorProfile) {
+
+        return null;
+    }
+
+
+    public ResonatorSummaryResponseDto getResonatorSummary() {
+
+        return null;
+    }
+
+
+    public ResonatorDetailResponseDto getResonatorDetail(Long userResonatorId) {
+
+        return null;
+    }
+
+
+    public void updateResonator(Long userResonatorId, UpdateResonatorRequestDto data) {
+
+    }
+
+    public void deleteResonator(DeleteResonatorRequestDto data) {
+
+    }
 }
